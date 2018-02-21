4aee360c9ad8764291c4db4891f035918798fd6d
James P. Fulford <james.patrick.fulford@gmail.com>
2018-02-21 00:18:46 -0500

    Fixed init

--- data/process/__init__.py
+++ data/process/__init__.py
@@ -1,8 +1,3 @@
 from stream import Stream
 
 import workers
-
-from worker_types import Worker
-from worker_types import IOWorker
-from worker_types import ThreadWorker
-# from worker_types import ProcessWorker
